b2e193c9fa6c82a2a5ce97a072d4b37e8a6b0a2e
Phimphichcha <89175207+Phimphichcha@users.noreply.github.com>
2021-11-22 14:02:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,10 +151,13 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if msg == "สวัสดี" :
              replyObj = TextSendMessage(text="ดี")
-        elif msg == "กินข้าวมั้ย" :
+             line_bot_api.reply_message(rtoken, replyObj)
+        elif msg == "กินข้าวมั้ย" 
              replyObj = TextSendMessage(text="ไม่เป็นไร")
+             line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "เบื่อ" :
              replyObj = TextSendMessage(text="แล้วมาบอกทำไม")
+             line_bot_api.reply_message(rtoken, replyObj)   
         else  :
              replyObj = TextSendMessage(text=msg)        
         line_bot_api.reply_message(rtoken, replyObj)
